a1bf12dbaf81f4ae0c934e8d0e0651a59a3d255a
Victor <victor.liger0800@gmail.com>
2024-02-03 20:58:58 -0300

    Adicionando sln

--- Hamburgueria/Models/Lanche.cs
+++ Hamburgueria/Models/Lanche.cs
@@ -2,5 +2,17 @@
 {
     public class Lanche
     {
+        public int LancheId { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnailUrl { get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool EmEstoque {  get; set; }
+        public Categoria Categoria { get; set; }
+        public int CategoriaId { get; set; }
+
     }
 }
